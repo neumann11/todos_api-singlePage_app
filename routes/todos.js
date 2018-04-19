9118f388ff922bf59ff157c97b0abcc10eba2f36
neumann11 <pavlo.vlasyuk@gmail.com>
2018-04-19 16:36:01 +0200

    Refactor routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,60 +1,15 @@
 var express = require("express");
 var router = express.Router(); //allows to brake routes into modular parts;
 var db = require("../models"); //autom. requires models/index.js
+var helpers = require("../helpers/todos");
 
-//INDEX ROUTES (SHOW ALL TODOS)
-router.get("/", function(req, res){
-    db.Todo.find()
-        .then(function(todos){
-            res.json(todos);
-        })
-        .catch(function(err){
-            res.send(err);
-        })
-});
+router.route("/")
+    .get(helpers.getTodos)
+    .post(helpers.createTodo);
 
-//CREATE ROUTE
-router.post("/", function(req, res){
-        db.Todo.create(req.body)
-            .then(function(newTodo) {
-                res.status(201).json(newTodo); // 201 - created.
-            })
-            .catch(function(err){
-                res.send(err);
-            })
-    });
-
-//SHOW ROUTE
-router.get("/:todoId", function(req, res){
-    db.Todo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-//UPDATE ROUTE
-router.put("/:todoId", function(req, res){
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true}) //{new: true} res.json with new data and not old.
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-//DELETE ROUTE
-router.delete("/:todoId", function(req, res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: "We deleted it!"});
-    })
-        .catch(function(err){
-            res.send(err);
-        })
-});
+router.route("/:todoId")
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo);
 
 module.exports = router;
