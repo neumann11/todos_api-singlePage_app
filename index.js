6463621b52ff8c0e8c3fe81df4b0bee6f7063ddd
neumann11 <pavlo.vlasyuk@gmail.com>
2018-04-23 11:58:29 +0200

    Add public directory, app.js, app.css, jQuery, AJAX jSON, views/index.html

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public")); //__dirname referes to the project directory name
+app.use(express.static(__dirname + "/views"));
+
 
 app.get("/", function(req, res){
-    res.send("HELLO FROM THE ROOT ROUTE!");
+    res.sendFile("index.html");
 });
 
 app.use("/api/todos", todoRoutes);
